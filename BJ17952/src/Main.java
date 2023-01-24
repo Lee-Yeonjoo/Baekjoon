9789c1e5e79953178d3a97a3e7849c672e0e3af7
Lee-Yeonjoo <121855519+Lee-Yeonjoo@users.noreply.github.com>
2023-01-25 02:29:47 +0900

    Add files via upload

---
+++ BJ17952/src/Main.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+import java.util.Stack;
+public class Main {
+    public static void main(String[] args){
+        int N;
+        int x;
+        int sum=0;
+        Scanner sc = new Scanner(System.in);
+        Stack<Integer> score = new Stack<>();
+        Stack<Integer> cnt = new Stack<>();
+        N = sc.nextInt();
+
+        for(int i=0; i<N; i++){
+            x = sc.nextInt();
+            if(x == 1){
+                score.push(sc.nextInt());
+                cnt.push(sc.nextInt());
+                if(!cnt.isEmpty()) {
+                    cnt.push(cnt.pop()-1);
+                    if (cnt.peek() == 0) {
+                        sum += score.pop();
+                        cnt.pop();
+                    }
+                }
+            }
+            else{
+                if(!cnt.isEmpty()) {
+                    cnt.push(cnt.pop()-1);
+                    if (cnt.peek() == 0) {
+                        sum += score.pop();
+                        cnt.pop();
+                    }
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+}
+
